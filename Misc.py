8d3f3215596ff5268529d8408d3e6119189cb795
Fredrik Pettersson <pettisson@hotmail.com>
2023-07-24 19:21:56 +0200

    "A third commit I guess" ^X

--- Misc.py
+++ Misc.py
@@ -1,5 +1,11 @@
 var_1 = [10, 34, 46, 78, 99]
 
 def add_one(x):
-    return x+10
+    temp_list = []
+    for item in x:
+        temp_list.append(item+10)
+    return temp_list
 
+var_2 = add_one(var_1)
+
+print(var_2)
